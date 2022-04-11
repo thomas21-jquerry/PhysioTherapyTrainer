3589f730b8b1dbfcf2462b5647fe3288a9c4b19d
ThomasJose1999 <thomas.jose@phaethon.tech>
2022-04-11 16:49:53 +0530

    added model2

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/physio-shoulder1',(req,res) => {
+    res.render('test.ejs');
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
